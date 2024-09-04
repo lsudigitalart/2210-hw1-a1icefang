bb5ba725eb8c76229d11078568e09d426b60aac5
John Doe <johndoe@example.com>
2024-09-04 11:42:01 -0500

    working now

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup () {
+    createCanvas (800,600)
+    ellipse(width / 2, height, 2, 200, 10);
+    ellipse(width / 2, height / 2, 100, 100);
+    ellipse(width / 2, height / 2, 10, 100);
+}
+
+function draw() {
+    
+}
