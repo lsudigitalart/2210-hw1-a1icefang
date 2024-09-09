202e03bc3d6decc97ef7b56ff28e8cc0c34b1d01
John Doe <johndoe@example.com>
2024-09-09 11:28:33 -0500

    weeee

--- sketch.js
+++ sketch.js
@@ -1,10 +1,49 @@
-function setup () {
-    createCanvas (800,600)
-    ellipse(width / 2, height, 2, 200, 10);
-    ellipse(width / 2, height / 2, 100, 100);
-    ellipse(width / 2, height / 2, 10, 100);
-}
+function setup() {
+    createCanvas(700, 800)
+    fill(258, 158, 54)
+    noStroke()
+    ellipse(width /2.24, height /1.99, 145, 85)
+    ellipse(width /1.43, height /1.99, 143, 85)
 
-function draw() {
+    fill(0, 0, 0)
+    ellipse(width /1.7, height /8, 150)
+
+    fill(255, 255, 255)
+    noStroke()
+    ellipse(width /1.65, height /8, 160)
+
+    fill(0, 0, 0)
+    ellipse(width /1.8, height /8, 74)
+
+    fill(255, 255, 255)
+    noStroke()
+    ellipse(width /1.76, height /8, 74)
     
+    fill(255, 255, 255)
+    noStroke()
+    ellipse(width /1.6, height /8, )
+
+    fill(219, 71, 7)
+    noStroke()
+    ellipse(width /1.75, height /3, 300)
+    ellipse(width /2.55, height /2.8, 120, 100)
+    ellipse(width /1.34, height /2.7, 120, 100)
+
+    fill(224, 224, 224)
+    noStroke()
+    ellipse(width /1.85, height /3.25, 200)
+
+    fill(3, 16, 156)
+    stroke (0, 0, 0)
+    strokeWeight (6)
+    ellipse(width /1.94, height /3.40, 140)
+
+    fill (0, 0, 0)
+    ellipse(width /1.98, height /3.75, 90)
+
+    fill (255, 255, 255)
+    noStroke()
+    ellipse(width /1.8, height /3, 45)
+    ellipse(width /2.1, height /3.8, 68)
+
 }
